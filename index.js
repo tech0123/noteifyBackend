ffe0f12af0de3a17f66e92a20a93ca6f4028c7e5
hd <classylifestyling@gmail.com>
2023-12-19 15:35:38 +0530

    deploy1

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ app.use(express.json()); //to get req body its middleware
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
-
 app.listen(port, () => {
   console.log(`Notes Backend listening on port ${port}`)
 })
